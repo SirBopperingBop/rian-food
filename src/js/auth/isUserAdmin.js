c91c5cb2a041276b78a83a4ff6a3fe4a818f0a3f
SirBopperingBop <matawifarookh@gmail.com>
2023-10-13 17:14:22 -0300

    egg

---
+++ src/js/auth/isUserAdmin.js
@@ -0,0 +1,51 @@
+import { supabase } from '../supabaseClient';
+
+const getAdminList = async () => {
+    try {
+        const { data, error } = await supabase
+            .from('admin_list')
+            .select('*');
+        
+        if (error) {
+            throw error;
+        }
+        
+        return data;
+    } catch (error) {
+        console.error('Error fetching admin list:', error);
+        throw error;
+    }
+};
+
+// Function to get the current user's session
+const getUserSession = async () => {
+    try {
+        const { data } = await supabase.auth.getSession();
+        return data ? data.session : null;
+    } catch (error) {
+        console.error('Error fetching user session:', error);
+        throw error;
+    }
+};
+
+// Function to check if the user is an admin
+export const isUserAdmin = async () => {
+    try {
+        const adminList = await getAdminList();
+        const currentSession = await getUserSession();
+
+        if (!adminList || !currentSession) {
+            console.warn('Admin list or user session not available.');
+            return false;
+        }
+
+        const userMetadataEmail = currentSession.user.user_metadata.email;
+
+        const isAdmin = adminList.some((admin) => admin.email === userMetadataEmail);
+
+        return isAdmin;
+    } catch (error) {
+        console.error('Error checking admin status:', error);
+        return false;
+    }
+};
